339ea1b956fd3c90255a2676c12295b25b14bef9
Unknown <ericbrailsford8@gmail.com>
2019-01-22 10:29:27 -0700

    First Line

---
+++ Adventure_Game/game.js
@@ -0,0 +1,16 @@
+// Single line comment
+/*
+
+multiline comment
+
+*/
+
+//alert("Warnign Will Robinson!");
+
+//confirm("Do ou like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("Hello Traveller, Whats your name?");
+
+document.write(pc);
