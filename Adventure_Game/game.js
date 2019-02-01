5b704a087288ba251737afd3c71b4f77c164c851
Unknown <ericbrailsford8@gmail.com>
2019-02-01 10:27:36 -0700

    oof

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -12,58 +12,81 @@ multiline comment
 //prompt("What type of pokemon do you like?");
 
 
-document.write(PlayerName);
+//document.write(PlayerName);
 
 Game();
 
 function Game(){
-    
-    alert("Povis, Land of Snow and Rocks");
-    document.write("Povis, Land of the Impossible")
-    var playerName = prompt("Hello Traveller, Whats your name?");
-    
-    alert("Welcome to Povis "+ playerName);
-    
-    village();
-    
-    function Village
-        var village = prompt("You awake from your sleep, on a hardfloor in an abandond building, you have no idea how you got there, and your confused. /n -look around /n -go back to sleep").toLowerCase();
-    
-        
-        if village == "look around" ||  village == "look"){
-            
-            var villageLook = prompt("The building is more of a shack. it has one room, the sun is shining through the rotting wood. and a nest of small insects in the corner of the square room. /n -eat bugs");
-        }
-            
-            else if(village == "go back to sleep" || village == "sleep"){
-                
-                alert("You fall back to sleep on the floor, you dream of a great land full of the impossible");
-                var resume = confirm("do you wish to continue?");
-                
-                if (resume){
-                    Village();
-                    
-                }
-                else{
-                    alert("Game Over");
-                }
-                }
-            Function Swamp(){
-                var swamp1 = prompt("You have enterted the swamp. /n -jump puddle /n -look around /n -follow road").toLowerCase();
-                
-                if(swamp1 == "jump puddle" || swamp1 == "jump"){
-                    alert("you were pulled into the puddle by a large tentacle...Game Over!");
-                }
-                
-                else if(swamp1 == "follow path" || swamp1 == "follow"){
-                    var swampPath = prompt("/n -go north /n -stop and rest");
-                    if(swampPath == "stop and rest"){
-                        alert("you rest your feet, and get captured by bandits...Game Over!");
-                    }
-                }
-                
-            }
-        
-    
-    
+	
+	alert("The Land of Povis!");
+	
+	var playerName = prompt("Welcome, Traveler! What is your name?");
+	
+	alert("Ah, Welcome to Povis, "+ playerName);
+	
+	House();
+	//House schtuff
+	function House(){
+		var house = prompt("You wake up in front of a house. \n -look around \n - go back to sleep \n - taunt the guard").toLowerCase();
+		
+		if(house == "look around" || house == "look"){
+			
+			var houseLook = prompt("the house is old, creaky, and has the doors nailed shut and a mound of bugs, you dont know what kind of bug they are. \n -open door \n -walk away \n -eat bugs");
+		}
+		
+		else if(house == "go back to sleep" || house == "sleep"){
+			alert("You fall back to sleep in the dirt. You dream of a land full of the impossible");
+			var resume = confirm("Do you wish to continue?");
+			
+			if(resume){
+				House();
+			}
+			else{
+				alert("Game Over!");
+			}
+		}
+		
+		else{
+			alert("I dont know what "+house+" is!");
+			House();
+		}
+	}
+	//Swamp Code
+	function Swamp(){
+		var swamp1 = prompt("This is a musty, monster ridden swamp. \n -follow path \n -swim");
+		
+		if(swamp1 == "follow" || swamp1 == "follow path"){
+			var swampPath = prompt("You enter on the swamp path and head north in the disance you see a witches hut. As you approach it you can hear the couldren bubbling, and her laugh growing ever more insane. \n -enter hut \n -burn hut");
+			if( swampPath = "enter"){
+				alert("You entered the hut. There is a Witch bent over a large cauldron on the fire in the center of the room.");
+			}	
+			else if(swampPath = "burn down hut"){
+				alert("You burn down the hut, you hear the Witch screaming in pain. she is dead.");
+			}	
+			else{
+				alert("dont throw rocks at hut...please!"); 
+			}	
+			
+			
+		}
+		else if(swamp1 == "Swim"){
+			
+		}
+		else{
+			alert("I don't understand "+swamp1);
+			Swamp();
+		}
+		
+		
+	} 
+	
+		
+		
+		
+	
+	
+	
+	
+	
+	
 }
